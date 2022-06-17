f7abbe6ce819113f68747b589f8ae01aec051d6f
eiwos <mahmoudi.otman@gmail.com>
2022-06-17 13:08:24 +0200

    Finished the getDataFromAFile and test about it

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Node.js modules imports
+ */
+
+/**
+ * Dependencies modules imports
+ */
+const dotenv = require("dotenv");
+
+/**
+ * Application modules Imports
+ */
+
+/**
+ * Application Initialisation
+ */
+dotenv.config();
+
+/**
+ * Application run
+ */
+console.log(process.env.DATAFOLDER)
