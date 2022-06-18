f9304a7b6a0cbfadc56a316c862f3a054cc625c2
eiwos <mahmoudi.otman@gmail.com>
2022-06-18 18:44:00 +0200

    deleted test about schema, not needed actually because deleted Schema parameter from parseRawData

--- src/services/parser/parser.service.js
+++ src/services/parser/parser.service.js
@@ -1,22 +1,20 @@
 "use strict";
-const { load } = require("cheerio");
+const { load, html } = require("cheerio");
+const { JSDOM } = require("jsdom");
 
 /**
  * @param {string} rawText - A string that represent the body or total html of a page
- * @param {object} schema - Rules for the parser to parse the data
  * @returns {Promise<void>}
  */
-const parseRawData = async (rawText, schema) => {
+const parseRawData = async (rawText) => {
   if (typeof rawText !== "string") {
     throw new TypeError(`${ parseRawData.name }: rawText is not a string.`);
   }
 
-  if (!(schema instanceof Object)) {
-    throw new TypeError(`${ parseRawData.name }: schema is not an object.`);
-  }
-
-  const rawHtml = load(rawText);
-  console.log(rawHtml.html());
+  // const newElement = new JSDOM(rawText);
+  // const document = newElement.window.document;
+  //
+  // return document.getElementById();
 };
 
 module.exports = {
--- src/services/parser/parser.service.spec.js
+++ src/services/parser/parser.service.spec.js
@@ -5,19 +5,8 @@ test(`func: ${ parseRawData.name } - Should throw a string TypeError`, async () 
   const someSchema = {};
 
   try {
-    await parseRawData(123, someSchema);
+    await parseRawData(123);
   } catch (err) {
     expect(err.message).toBe(`${ parseRawData.name }: rawText is not a string.`);
-  }
-});
-
-test(`func: ${ parseRawData.name } - Should throw a Object TypeError`, async () => {
-  const someTxt = "<p></p>";
-  const someSchema = {};
-
-  try {
-    await parseRawData(someTxt, 124587);
-  } catch (err) {
-    expect(err.message).toBe(`${ parseRawData.name }: schema is not an object.`);
   }
 });
